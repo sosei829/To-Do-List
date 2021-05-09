7d50c6311684cc66ac0b3824b952dbeb8fe7f273
Lin <sosei829@hotmaiil.com>
2021-05-09 16:10:31 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+// 載入 mongoose並連線到 mongoDB
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//把連線狀態暫存下來
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
