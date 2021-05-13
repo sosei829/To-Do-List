f8ac732d7031ff5b820e69b38d3841e50817b0f2
Lin <sosei829@hotmaiil.com>
2021-05-13 20:29:23 +0800

    feat: add sort & switch function

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({_id:'asc'})
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
@@ -65,10 +66,11 @@ app.get('/todos/:id/edit', (req, res) => {
 
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
     .then(()=> res.redirect(`/todos/${id}`))
--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,11 @@ const todoSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
+  },
+  isDone:{
+    type:Boolean,
+    default: false
   }
+
 })
 module.exports = mongoose.model('Todo', todoSchema)
